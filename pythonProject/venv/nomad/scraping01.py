75e957d4031b1aadb3de0dc64bb210deff99d60f
sirius <seamus-1@naver.com>
2021-01-11 14:55:55 +0900

    extrack page

--- pythonProject/venv/nomad/scraping01.py
+++ pythonProject/venv/nomad/scraping01.py
@@ -10,15 +10,21 @@ indeed_result = requests.get("https://www.indeed.com/jobs?q=python&limit=50") # 
 indeed_soup = BeautifulSoup(indeed_result.text, "html.parser") #show how many data in page explore and extract data
 # print(indeed_soup)
 
-pagination = indeed_soup.find("div", {"class": "pagination"})
+pagination = indeed_soup.find("div", {"class": "pagination"}) # whatever we find "div","class" inside of indeed_soup
 # print(pagination)
-pages = pagination.find_all('a')
+links = pagination.find_all('a') #we find all the links 'a'
 # print(pages)
-spans =[]
-for page in pages: #여기에서 pages는 리스트이다.
+pages=[]
+for link in links[:-1]: #여기에서 pages는 리스트이다.
     # print(page.find("span"))
-    spans.append(page.find("span"))
+    pages.append(int(link.string))
     #array를 만들고 span 을 찾을 때마다 array에 넣어준
-print(spans[:-1])#  -1은 마지막에 시작해 첫번째 아이템을 나타내준
+#print(pages[:-1])#  -1은 마지막에 시작해 첫번째 아이템을 나타내준
 
 #첫번째 과정 끝!! 페이지 추출하기
+max_page =pages[-1]# find last page
+
+#requests 보내기
+
+for n in range(max_page):
+    print(f'start={n*50}') #range의 현재 값을 indeed에서 가져온 요소 개수만큼 곱해보자
